7475a6b82c855671dd1fdbd846a32f1d17a8162e
jay77721 <jy77721@gmail.com>
2026-03-25 17:11:40 +0800

    feat: allow OAuth users to self-register when general registration is disabled (#8042)

--- resources/views/livewire/settings/advanced.blade.php
+++ resources/views/livewire/settings/advanced.blade.php
@@ -18,7 +18,7 @@
                 <div class="flex flex-col gap-1">
                     <div class="md:w-96">
                         <x-forms.checkbox instantSave id="is_registration_enabled"
-                            helper="Allow users to self-register. If disabled, only administrators can create accounts."
+                            helper="Allow users to self-register. If disabled, only administrators can create accounts. OAuth users can always self-register."
                             label="Registration Allowed" />
                     </div>
                     <div class="md:w-96">
